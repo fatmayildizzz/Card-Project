f6732963874cb2a4902324841393567b09c7336a
fatmayildizzz <ftmayildiz60@gmail.com>
2022-09-02 14:35:49 +0300

    Master

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'kart';
+  
+
+  posts=[
+    {
+      title: 'Dag Bisikleti',
+      imageUrl:'assets/biking.jpeg',
+      content:'Bugun bisikleti guzel surdum',
+      userName:'Fatma'
+    },
+    {
+      title: 'Tirmanis',
+      imageUrl:'assets/mountain.jpeg',
+      content:'Bugun iyi tirmandim',
+      userName:'Sevilay'
+
+    },
+    {
+      title: 'Doga Yuruyusu',
+      imageUrl:'assets/tree.jpeg',
+      content:'Hadi biraz macka parkinda yuruyelim',
+      userName:'Erdem'
+    }
+  ]
 }
